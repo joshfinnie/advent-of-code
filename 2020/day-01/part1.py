47f121bdda1bb3665f432d5df4c48eb311a475e1
Josh Finnie <josh@jfin.us>
2020-12-01 12:51:12 -0500

    day 1 in python

---
+++ 2020/day-01/part1.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def get_values(arr):
+    sum = 2020
+    for i in arr:
+        for j in arr:
+            if (i + j == sum):
+                return i * j
+
+values = []
+with open(sys.argv[1]) as inputfile:
+    for lines in inputfile:
+        values.append(int(lines))
+
+print(get_values(values))
---
+++ 2020/day-01/part2.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def get_values(arr):
+    sum = 2020
+    for i in arr:
+        for j in arr:
+            for z in arr:
+                if (i + j + z == sum):
+                    return i * j * z
+
+values = []
+with open(sys.argv[1]) as inputfile:
+    for lines in inputfile:
+        values.append(int(lines))
+
+print(get_values(values))
