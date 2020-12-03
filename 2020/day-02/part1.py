ad6891b477e807d9e10128325e977737c5bb1acc
Josh Finnie <josh@jfin.us>
2020-12-03 10:10:17 -0500

    day 2

---
+++ 2020/day-02/part1.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def confirm_password(rule):
+    count, letter, password = rule.split(" ")
+    minimum, maximum = count.split("-")
+    letter = letter.strip(":")
+
+    letter_count = password.count(letter)
+
+    if (letter_count >= int(minimum) and letter_count <= int(maximum)):
+        return 1
+    else:
+        return 0
+
+
+values = []
+with open(sys.argv[1]) as inputfile:
+    for line in inputfile:
+        values.append(line.strip())
+
+totals = 0
+for p in values:
+    totals += confirm_password(p)
+print(totals)
---
+++ 2020/day-02/part2.py
@@ -0,0 +1,27 @@
+import sys
+
+
+def confirm_password(rule):
+    count, letter, password = rule.split(" ")
+    minimum, maximum = count.split("-")
+    letter = letter.strip(":")
+
+    first = password[int(minimum)-1]
+    last = password[int(maximum)-1]
+
+    if ((first == letter and last != letter) or (first != letter and last == letter)):
+        return 1
+    else:
+        return 0
+
+
+values = []
+with open(sys.argv[1]) as inputfile:
+    for line in inputfile:
+        values.append(line.strip())
+
+#values = ["1-3 a: abcde","1-3 b: cdefg","2-9 c: ccccccccc"]
+totals = 0
+for p in values:
+    totals += confirm_password(p)
+print(totals)
