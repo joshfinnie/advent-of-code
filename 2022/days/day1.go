6ed97cd433c88e5c36889c02c46974831e717479
Josh Finnie <jfinnie@indigoag.com>
2022-12-01 09:13:21 -0500

    2022 day 01

--- 2022/days/day1.go
+++ 2022/days/day1.go
@@ -1,9 +1,40 @@
 package days
 
+import (
+	"math"
+	"sort"
+	"strings"
+	"strconv"
+)
+
+func parse(input string) []int {
+	input = strings.TrimSuffix(input, "\n")
+	parts := strings.Split(input, "\n\n")
+	elves := make([]int, 0)
+	for _, part := range parts {
+		sum := 0
+		for _, line := range strings.Split(part, "\n") {
+			i, _ := strconv.Atoi(line)
+			sum += i
+		}
+		elves = append(elves, sum)
+	}
+	return elves
+}
+
 func Day01Part1(input string) string {
-	return "answer to day 01 part 1"
+	elves := parse(input)
+	max := math.MinInt
+	for _, elf := range elves {
+		if elf > max {
+			max = elf
+		}
+	}
+	return strconv.Itoa(max)
 }
 
 func Day01Part2(input string) string {
-	return "answer to day 01 part 2"
+	elves := parse(input)
+	sort.Sort(sort.Reverse(sort.IntSlice(elves)))
+	return strconv.Itoa(elves[0] + elves[1] + elves[2])
 }
