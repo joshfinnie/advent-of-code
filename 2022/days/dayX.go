089dc6f764ed7af90ecfd76586e1042840c58432
Josh Finnie <josh@jfin.us>
2022-12-02 23:38:51 -0500

    adding a generic day to copy

---
+++ 2022/days/dayX.go
@@ -0,0 +1,19 @@
+package days
+
+import (
+	"strconv"
+	"strings"
+)
+
+func parse(input string) string {
+	input = strings.TrimSuffix(input, "\n")
+	return input
+}
+
+func DayXXPart1(input string) string {
+	return strconv.Itoa(0)
+}
+
+func DayXXPart2(input string) string {
+	return strconv.Itoa(0)
+}
