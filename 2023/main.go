a03832f86a62673e2282577feac867b488d2611a
Josh Finnie <josh.finnie@peopledatalabs.com>
2023-12-12 10:23:34 -0500

    day 7

--- 2023/main.go
+++ 2023/main.go
@@ -21,6 +21,7 @@ var dayFunctions = map[string]struct {
 	"04": {days.Day04A, days.Day04B},
 	"05": {days.Day05A, days.Day05B},
 	"06": {days.Day06A, days.Day06B},
+	"07": {days.Day07A, days.Day07B},
 }
 
 // getDay returns the function corresponding to the specified day and part2 flag.
